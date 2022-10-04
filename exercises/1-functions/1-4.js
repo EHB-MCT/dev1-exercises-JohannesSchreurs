5033e1cb6fcb1ce40f1a3d0bdbd18663dca2fcf6
Johannes Schreurs <johannesschreurs@icloud.com>
2022-10-04 19:20:11 +0200

    add fifth ex

---
+++ exercises/1-functions/1-4.js
@@ -0,0 +1,33 @@
+"use strict";
+
+window.onresize = draw;
+
+function draw () {
+    let canvas = document.querySelector('canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+    // Sky
+    context.beginPath();
+    context.rect(50, 50, 500, 500);
+    context.fillStyle = 'orange';
+    context.fill();
+    context.closePath();
+    
+    // Sea
+    context.beginPath();
+    context.rect(50, 350, 500, 200);
+    context.fillStyle = 'aqua';
+    context.fill();
+    context.closePath();
+
+    // Sun
+    context.beginPath();
+    context.ellipse(300, 350, 200, 200, Math.PI, 0, Math.PI);
+    context.fillStyle = 'yellow';
+    context.fill();
+    context.closePath();
+}
+
+draw();
